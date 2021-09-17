9b8a3ab7586e3d5e25cacc465fcda5e7765964a5
blacktoast <dbstjd100@naver.com>
2021-09-17 16:35:21 +0900

    2 files modified

--- index.ts
+++ index.ts
@@ -4,3 +4,17 @@ import './style.css';
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+const title: HTMLElement = document.querySelector('.title');
+
+function handleCilck() {
+  console.log('click');
+  title.style.color = 'red';
+}
+function handleMouseEnter() {
+  console.log('click');
+}
+console.log(title);
+
+title.addEventListener('click', handleCilck);
+title.addEventListener('mouseenter', handleMouseEnter);
